b69bf0eb1a017e155b3860c33208a909a2782cbe
Christian Ang <cang@pivotal.io>
2017-11-28 17:44:30 -0800

    Out returns empty version

--- api/request.go
+++ api/request.go
@@ -5,7 +5,16 @@ type InRequest struct {
 	Version map[string]string `json:"version"`
 }
 
+type OutRequest struct {
+	Params  OutParams         `json:"params"`
+	Source  RequestSource     `json:"source"`
+	Version map[string]string `json:"version"`
+}
+
 type RequestSource struct {
 	Filename string `json:"filename"`
 	Data     string `json:"data"`
+}
+
+type OutParams struct {
 }
--- cmd/out/main.go
+++ cmd/out/main.go
@@ -1,4 +1,19 @@
 package main
 
+import (
+	"encoding/json"
+	"log"
+	"os"
+
+	"github.com/christianang/staticfile-resource/api"
+)
+
 func main() {
+	var outRequest api.OutRequest
+	err := json.NewDecoder(os.Stdin).Decode(&outRequest)
+	if err != nil {
+		log.Fatal("failed to decode: ", err)
+	}
+
+	json.NewEncoder(os.Stdout).Encode(api.Response{})
 }
