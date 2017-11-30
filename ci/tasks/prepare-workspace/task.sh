d05db809d53021b97e7f4db111882ce1ff981c96
Christian Ang <cang@pivotal.io>
2017-11-29 17:24:02 -0800

    Download dep to download dependencies in ci

--- ci/tasks/prepare-workspace/task.sh
+++ ci/tasks/prepare-workspace/task.sh
@@ -5,9 +5,12 @@ GOPATH=${PWD}/go
 PATH=${GOPATH}/bin:$PATH
 
 function main() {
+  go get github.com/golang/dep/cmd/dep
+
   mkdir -p "${GOPATH}/src/github.com/christianang"
   ln -s "${ROOT}/staticfile-resource" "${GOPATH}/src/github.com/christianang/staticfile-resource"
   pushd "${GOPATH}/src/github.com/christianang/staticfile-resource" > /dev/null
+    dep ensure
     ./scripts/build
   popd > /dev/null
 
--- ci/tasks/run-tests/task.sh
+++ ci/tasks/run-tests/task.sh
@@ -6,6 +6,7 @@ PATH=${GOPATH}/bin:$PATH
 
 function main() {
   go get github.com/onsi/ginkgo/ginkgo
+  go get github.com/golang/dep/cmd/dep
 
   mkdir -p "${GOPATH}/src/github.com/christianang"
   ln -s "${ROOT}/staticfile-resource" "${GOPATH}/src/github.com/christianang/staticfile-resource"
