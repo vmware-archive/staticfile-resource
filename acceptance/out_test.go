86b3d264a903db90993283c6bc2cd618f88c19cc
Christian Ang <cang@pivotal.io>
2017-11-29 16:19:11 -0800

    Add acceptance test for out

---
+++ acceptance/out_test.go
@@ -0,0 +1,35 @@
+package acceptance_test
+
+import (
+	"io"
+	"os"
+	"os/exec"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Out", func() {
+	It("returns an empty version", func() {
+		out := exec.Command(pathToOut)
+		out.Stderr = os.Stderr
+
+		stdin, err := out.StdinPipe()
+		Expect(err).NotTo(HaveOccurred())
+
+		_, err = io.WriteString(stdin, `{
+			"source": {
+				"filename": "some-filename",
+				"data": "some-data"
+			},
+			"version": {}
+		}`)
+		Expect(err).NotTo(HaveOccurred())
+
+		output, err := out.Output()
+		Expect(err).NotTo(HaveOccurred())
+		Expect(output).To(MatchJSON(`{
+			"version": {}
+		}`))
+	})
+})
