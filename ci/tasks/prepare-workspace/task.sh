e91c7750ffb3431cd16f7429ff8e84b62dee6bf6
Christian Ang <cang@pivotal.io>
2017-11-29 17:20:30 -0800

    Fix ci scripts

--- ci/tasks/prepare-workspace/task.sh
+++ ci/tasks/prepare-workspace/task.sh
@@ -5,9 +5,9 @@ GOPATH=${PWD}/go
 PATH=${GOPATH}/bin:$PATH
 
 function main() {
-  mkdir -p "${GOPATH}/src/github.com/pivotal-cf"
-  ln -s "${ROOT}/staticfile-resource" "${GOPATH}/src/github.com/pivotal-cf/staticfile-resource"
-  pushd "${GOPATH}/src/github.com/pivotal-cf/staticfile-resource" > /dev/null
+  mkdir -p "${GOPATH}/src/github.com/christianang"
+  ln -s "${ROOT}/staticfile-resource" "${GOPATH}/src/github.com/christianang/staticfile-resource"
+  pushd "${GOPATH}/src/github.com/christianang/staticfile-resource" > /dev/null
     ./scripts/build
   popd > /dev/null
 
--- ci/tasks/run-tests/task.sh
+++ ci/tasks/run-tests/task.sh
@@ -7,9 +7,10 @@ PATH=${GOPATH}/bin:$PATH
 function main() {
   go get github.com/onsi/ginkgo/ginkgo
 
-  mkdir -p "${GOPATH}/src/github.com/pivotal-cf"
-  ln -s "${ROOT}/staticfile-resource" "${GOPATH}/src/github.com/pivotal-cf/staticfile-resource"
-  ginkgo -r "${GOPATH}/src/github.com/pivotal-cf/staticfile-resource"
+  mkdir -p "${GOPATH}/src/github.com/christianang"
+  ln -s "${ROOT}/staticfile-resource" "${GOPATH}/src/github.com/christianang/staticfile-resource"
+  dep ensure "${GOPATH}/src/github.com/christianang/staticfile-resource"
+  ginkgo -r "${GOPATH}/src/github.com/christianang/staticfile-resource"
 }
 
 main
